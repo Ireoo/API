6b07653f0535f52b6f12bf84832703c926d23710
S2 <s@ireoo.com>
2018-09-28 15:31:29 +0800

    修改默认配置

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 	/**
 	 * 获取mongodb数据库参数
 	 */
-	let connect = process.env.MONGODB || "127.0.0.1:27017/api";
+	let connect = process.env.MONGODB || "127.0.0.1:27017/admin";
 
 	/**
 	 * 设置mongodb数据库连接
