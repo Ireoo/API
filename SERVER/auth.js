945ec51deffa2f1710185e0e814be86cb0efb55d
S2 <i@ireoo.com>
2018-09-29 21:20:11 +0800

    优化程序

--- SERVER/auth.js
+++ SERVER/auth.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require("express");
 const router = express.Router();
 const md5 = require("md5");
@@ -8,11 +11,23 @@ exports = module.exports = router;
  * 首页内容
  */
 router.post("/", (req, res, next) => {
-	console.log(req.body, req.params, req.query, req.data);
-	if (!req.header("Authorization") || req.header("Authorization") === "")
-		return res.status(503).send("No Authorization.");
+    console.log(req.body, req.params, req.query, req.data);
+    if (!req.header("Authorization") || req.header("Authorization") === "")
+        return res.status(200).send("没有设置用户验证密钥,此项功能需要用户验证信息!");
 
-	let code = req.header("Authorization");
+    let code = req.header("Authorization");
 
-	next();
+    let file = path.join(process.cwd(), `sessions/${code}.json`);
+    if (fs.existsSync(file)) {
+        let session = JSON.parse(fs.readFileSync(file));
+        if (new Date().getTime() - session.timer < 24 * 60 * 60 * 1000) {
+            req.session = session;
+            next();
+        } else {
+            res.status(200).send(`用户信息已经过期,请重新登陆授权!`);
+        }
+    } else {
+        res.status(200).send(`请确认是否已经登陆,并获取正确的验证密钥!`);
+    }
+    // next();
 });
--- routes/authorize.js
+++ routes/authorize.js
@@ -6,16 +6,19 @@ router.auth = false;
 // router.path = "/";
 
 router.all("/", (req, res, next) => {
-	console.log(req.body, req.params, req.query, req.data);
-	let app = req.data;
+    let username = req.data.username,
+        password = req.data.password;
 
-	if (app.id && app.secret) {
-		res.send({
-			success: true
-		});
-	} else {
-		res.send({
-			success: false
-		});
-	}
+    if (username && username !== '' && password && password !== '') {
+        res.send({
+            success: true,
+            access_token: code,
+            timer: new Date().getTime()
+        });
+    } else {
+        res.send({
+            success: false,
+            data: '请确认你的账号和密码是否正确填写!'
+        });
+    }
 });
