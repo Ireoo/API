6bfdcdfa3587b4cd39c962697682f70a87f9b79e
S2 <i@ireoo.com>
2018-09-30 21:13:51 +0800

    重新设置核心文件

--- SERVER/auth.js
+++ SERVER/auth.js
@@ -13,20 +13,27 @@ exports = module.exports = router;
 router.post("/", (req, res, next) => {
 	let code = req.header("Authorization") || req.header("authorization");
 
-	if (!code || code === "") return res.status(200).send("没有设置用户验证密钥,此项功能需要用户验证信息!");
+	if (!code || code === "")
+		return res.status(200).send({
+			success: false,
+			data: "没有设置应用验证密钥!"
+		});
 
-	let file = path.join(process.cwd(), `sessions/${code}.json`);
-	if (fs.existsSync(file)) {
-		let session = JSON.parse(fs.readFileSync(file));
-		if (new Date().getTime() - session.timer < 24 * 60 * 60 * 1000) {
-			req.session = session;
+	req.mongodb["app"].findOne(
+		{
+			secret
+		},
+		{},
+		(error, data) => {
+			if (error)
+				return res.status(503).send({
+					success: false,
+					data: error
+				});
+			req.app = data;
 			next();
-		} else {
-			res.status(200).send(`用户信息已经过期,请重新登陆授权!`);
 		}
-	} else {
-		res.status(200).send(`请确认是否已经登陆,并获取正确的验证密钥!`);
-	}
+	);
 
 	// next();
 });
