4916267df7e7fff8cd4d00e6f026e1e4b152883c
Ireoos <s@ireoo.com>
2018-10-02 16:33:12 +0800

    add heroku config mongodb uri

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 	/**
 	 * 获取mongodb数据库参数
 	 */
-	let connect = process.env.MONGODB || "127.0.0.1/api";
+	let connect = process.env.MONGODB_URI || process.env.MONGODB || "127.0.0.1/api";
 
 	/**
 	 * 设置mongodb数据库连接
