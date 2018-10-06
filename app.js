4ab8aef0d04541319ede31e930dd2994a2377ccd
S2 <s@ireoo.com>
2018-10-07 00:43:41 +0800

    优化数据连接

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ app.use(compression());
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	res.header("Access-Control-Allow-Headers", "Content-Type");
-	res.header("Access-Control-Allow-Headers", "Authorization");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
 	if (req.header("Access-Control-Allow-Methods") === "OPTIONS") {
 		res.status(200).send(null);
 	} else {
