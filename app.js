74a96a65507115d0a45381b108d0e29e3473501d
Jack Wang <jack.wang@integem.com>
2018-10-27 23:15:37 +0800

    优化程序

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ const MongoDB = require("./lib/database");
 /**
  * 显示访问信息
  */
-// app.use(
-// 	morgan(
-// 		'[:date[web]] :remote-addr - :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" - :response-time ms'
-// 	)
-// );
+app.use(
+	morgan(
+		'[:date[web]] :remote-addr - :remote-user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" - :response-time ms'
+	)
+);
 
 /**
  * 添加gzip
@@ -26,61 +26,61 @@ app.use(compression());
  * 允许跨域访问
  */
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
 
-    if (req.method === "OPTIONS") {
-        res.status(200).send(null);
-    } else {
-        next();
-    }
+	if (req.method === "OPTIONS") {
+		res.status(200).send(null);
+	} else {
+		next();
+	}
 });
 
 /**
  * 设置数据库
  */
 app.use((req, res, next) => {
-    /**
-     * 获取mongodb数据库参数
-     */
-    let mongodb_other = process.env.MONGODB_OTHER ? `?${process.env.MONGODB_OTHER}` : "";
-    let connect = `${process.env.MONGODB}/api${mongodb_other}` || "127.0.0.1/api";
+	/**
+	 * 获取mongodb数据库参数
+	 */
+	let mongodb_other = process.env.MONGODB_OTHER ? `?${process.env.MONGODB_OTHER}` : "";
+	let connect = `${process.env.MONGODB}/api${mongodb_other}` || "127.0.0.1/api";
 
-    /**
-     * 设置mongodb数据库连接
-     */
-    // req.mongodb = mongojs(connect);
-    req.api = new MongoDB(connect);
-    next();
+	/**
+	 * 设置mongodb数据库连接
+	 */
+	// req.mongodb = mongojs(connect);
+	req.api = new MongoDB(connect);
+	next();
 });
 
 /**
  * 获取数据流并保存在 req.data 里面
  */
 app.use((req, res, next) => {
-    let reqData = [];
-    let size = 0;
-    req.on("data", data => {
-        reqData.push(data);
-        size += data.length;
-    });
-    req.on("end", () => {
-        try {
-            req.data = JSON.parse(
-                Buffer.concat(reqData, size).toString() === "" ? "{}" : Buffer.concat(reqData, size).toString()
-            );
-            next();
-        } catch (error) {
-            res.send({
-                success: false,
-                message: `提交的数据格式错误,请提交json格式的文本`,
-                data: Buffer.concat(reqData, size).toString
-            });
-            // req.data = {};
-            // next();
-        }
-    });
+	let reqData = [];
+	let size = 0;
+	req.on("data", data => {
+		reqData.push(data);
+		size += data.length;
+	});
+	req.on("end", () => {
+		try {
+			req.data = JSON.parse(
+				Buffer.concat(reqData, size).toString() === "" ? "{}" : Buffer.concat(reqData, size).toString()
+			);
+			next();
+		} catch (error) {
+			res.send({
+				success: false,
+				message: `提交的数据格式错误,请提交json格式的文本`,
+				data: Buffer.concat(reqData, size).toString
+			});
+			// req.data = {};
+			// next();
+		}
+	});
 });
 
 // app.use((req, res, next) => {
@@ -97,12 +97,17 @@ app.use("/", require("./SERVER/routes"));
  * 设置服务器端口默认为80
  */
 const server = app.listen(process.env.PORT || 2018, () => {
-    console.log("Listening on port %s:%d", server.address().address, server.address().port);
+	console.log(
+		"Listening on port %s:%d[%s]",
+		server.address().address,
+		server.address().port,
+		server.address().family
+	);
 });
 
 /**
  * 处理错误
  */
 process.on("uncaughtException", error => {
-    console.log("Caught exception: ", error);
+	console.log("Caught exception: ", error);
 });
