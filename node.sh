af12bc18289d77f017867d49f5196c99ecad1ed3
Ireoo <s@ireoo.com>
2018-11-24 16:25:35 +0000

    更新node.sh, README.md, .gitlab-ci.yml 已删除daocloud.yml, wercker.yml

--- node.sh
+++ node.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo apt-get install yarn
