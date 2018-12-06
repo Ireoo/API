168d01bc59cdf8e91c4f02b343756ce097a8b0c5
Ireoo <s@ireoo.com>
2018-12-06 07:36:56 +0000

    更新bash/certs.sh

--- bash/certs.sh
+++ bash/certs.sh
@@ -4,16 +4,16 @@ else
     read -p "请输入绑定域名:" SITE;
 fi
 
-apt update
-apt upgrade -y
-apt install letsencrypt nginx -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt install letsencrypt nginx -y
 
-service nginx stop
-letsencrypt certonly --standalone --agree-tos --email s@ireoo.com -d $SITE
-service nginx start
+sudo service nginx stop
+sudo letsencrypt certonly --standalone --agree-tos --email s@ireoo.com -d $SITE
+sudo service nginx start
 
-sed -i 's/api.qiyi.io/$SITE/g' ./nginx/api.conf
-cp ./nginx/api.conf /etc/nginx/conf.d/api.conf
-nginx -s reload
+sudo sed -i 's/api.qiyi.io/$SITE/g' ./nginx/api.conf
+sudo cp ./nginx/api.conf /etc/nginx/conf.d/api.conf
+sudo nginx -s reload
 
-echo '* 5 * * * root service nginx stop && letsencrypt renew && service nginx start' >> /etc/crontab
+sudo echo '* 5 * * * root service nginx stop && letsencrypt renew && service nginx start' >> /etc/crontab
