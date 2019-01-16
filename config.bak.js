5c3f1e8dfecc05abd76d964490af367a694c7cb8
Ireoo <s@ireoo.com>
2019-01-17 01:29:22 +0800

    Update config.bak.js

--- config.bak.js
+++ config.bak.js
@@ -1,5 +1,8 @@
+const MONGO = process.env.MONGO_PORT_27017_TCP_ADDR ? `${process.env.MONGO_ENV_MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_ENV_MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_PORT_27017_TCP_ADDR}:${process.env.MONGO_PORT_27017_TCP_PORT}` : NULL
+if(MONGO) const OTHER = "authSource=admin"
+
 exports = module.exports = {
-	MONGODB: process.env.MONGODB || "localhost",
+	MONGODB: MONGO || process.env.MONGODB || "localhost",
 	MONGODB_OTHER: process.env.MONGODB_OTHER || "",
 	PORT: process.env.PORT || 2012,
 	SECRET: process.env.SECRET || "94f3eee0-218f-41fc-9318-94cf5430fc7f"
