b30c9b6d3999b78cb5e22d4d5efbc04b52e4eb4e
S2 <s@ireoo.com>
2019-03-15 21:23:22 +0800

    update

--- bash/docker.sh
+++ bash/docker.sh
@@ -1,7 +1,19 @@
-sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
-docker -v
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo systemctl restart docker
+sudo docker version
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
