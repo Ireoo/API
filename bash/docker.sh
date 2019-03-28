fe0202816b706ee973c1d5f2ccea0b7c27179dad
S2 <s@ireoo.com>
2019-03-28 14:08:50 +0800

    更新安装脚本

--- bash/docker.sh
+++ bash/docker.sh
@@ -12,3 +12,6 @@ sudo docker version
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
+cd docker
+docker-compose up -d
